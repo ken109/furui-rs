17fa1b2e4de760277b442bd39b4cb23783a3588f
Kensuke Kubo <kensukekubo19@gmail.com>
2022-04-18 01:55:31 +0900

    load programs

---
+++ furui-ebpf/src/bind.rs
@@ -0,0 +1,37 @@
+#![no_std]
+#![no_main]
+
+use aya_bpf::{
+    macros::kprobe,
+    programs::ProbeContext,
+};
+
+#[kprobe(name = "bind_v4")]
+pub fn bind_v4(ctx: ProbeContext) -> u32 {
+    match unsafe { try_bind_v4(ctx) } {
+        Ok(ret) => ret,
+        Err(ret) => ret,
+    }
+}
+
+unsafe fn try_bind_v4(_ctx: ProbeContext) -> Result<u32, u32> {
+    Ok(0)
+}
+
+#[kprobe(name = "bind_v6")]
+pub fn bind_v6(ctx: ProbeContext) -> u32 {
+    match unsafe { try_bind_v6(ctx) } {
+        Ok(ret) => ret,
+        Err(ret) => ret,
+    }
+}
+
+unsafe fn try_bind_v6(_ctx: ProbeContext) -> Result<u32, u32> {
+    Ok(0)
+}
+
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    unsafe { core::hint::unreachable_unchecked() }
+}
