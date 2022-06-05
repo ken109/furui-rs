2081dfe389e43c0771a1e9ee6dbc7cb8f36d4c3f
Kensuke Kubo <kensukekubo19@gmail.com>
2022-06-06 00:50:54 +0900

    実行中のコンテナ名でリモートホストを指定できる

---
+++ furui/src/docker.rs
@@ -0,0 +1,72 @@
+use std::collections::HashMap;
+use std::net::IpAddr;
+
+use anyhow::anyhow;
+use bollard;
+use bollard::container::ListContainersOptions;
+use bollard::models::{ContainerInspectResponse, ContainerSummary};
+use log::warn;
+
+use crate::constant::CONTAINER_ID_LENGTH;
+use crate::domain::{Container, Containers};
+
+pub struct Docker {
+    docker: bollard::Docker,
+}
+
+impl Docker {
+    pub fn new() -> anyhow::Result<Docker> {
+        Ok(Docker { docker: bollard::Docker::connect_with_local_defaults()? })
+    }
+
+    async fn containers(&self) -> anyhow::Result<Vec<ContainerSummary>> {
+        let mut list_container_filters = HashMap::new();
+        list_container_filters.insert("status", vec!["running"]);
+
+        Ok(self.docker
+            .list_containers(Some(ListContainersOptions {
+                all: true,
+                filters: list_container_filters,
+                ..Default::default()
+            }))
+            .await?)
+    }
+
+    pub async fn set_container_inspect(&self, container: &mut Container) -> anyhow::Result<()> {
+        let inspect = self.docker.inspect_container(&container.id.as_ref().unwrap(), None).await?;
+
+        let mut addrs: Vec<IpAddr> = vec![];
+        for (_, network) in inspect.network_settings.unwrap().networks.unwrap() {
+            match network.ip_address.unwrap().parse::<IpAddr>() {
+                Ok(addr) => addrs.push(addr),
+                Err(_) => {}
+            };
+
+            match network.global_ipv6_address.unwrap().parse::<IpAddr>() {
+                Ok(addr) => addrs.push(addr),
+                Err(_) => {}
+            };
+        }
+
+        container.id = Some(container.id.as_ref().unwrap().chars().take(CONTAINER_ID_LENGTH).collect::<String>());
+        container.ip_addresses = Some(addrs);
+        container.name = inspect.name.unwrap();
+        container.pid = inspect.state.unwrap().pid.unwrap();
+
+        Ok(())
+    }
+
+    pub async fn add_running_containers_inspect(&self, containers: &mut Containers) -> anyhow::Result<()> {
+        let docker_containers = self.containers().await?;
+
+        for docker_container in docker_containers {
+            let mut container = Container::new(docker_container.id.unwrap());
+
+            self.set_container_inspect(&mut container).await?;
+
+            containers.add(container);
+        }
+
+        Ok(())
+    }
+}
--- furui/src/domain/container.rs
+++ furui/src/domain/container.rs
@@ -2,7 +2,47 @@ use std::net::IpAddr;
 
 #[derive(Debug, Clone)]
 pub struct Container {
-    pub(crate) id: Option<String>,
-    pub(crate) ip_addresses: Option<IpAddr>,
-    pub(crate) name: String,
+    pub id: Option<String>,
+    pub ip_addresses: Option<Vec<IpAddr>>,
+    pub name: String,
+    pub pid: i64,
+}
+
+impl Container {
+    pub fn new(id: String) -> Container {
+        Container {
+            id: Some(id),
+            ip_addresses: None,
+            name: "".to_string(),
+            pid: 0,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Containers {
+    containers: Vec<Container>,
+}
+
+impl Containers {
+    pub fn new() -> Containers {
+        Containers { containers: vec![] }
+    }
+
+    pub fn add(&mut self, container: Container) {
+        self.containers.push(container)
+    }
+
+    pub fn get_container_by_name(&self, name: &str) -> Option<Container> {
+        for container in &self.containers {
+            let mut container_name = container.name.clone();
+
+            container_name.remove(0);
+
+            if container_name == name {
+                return Some(container.clone());
+            }
+        }
+        None
+    }
 }
