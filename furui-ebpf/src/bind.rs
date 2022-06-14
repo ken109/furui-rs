fe12ce6b803ffd666eee18bd3eae9d3ac9358c60
Kensuke Kubo <kensukekubo19@gmail.com>
2022-06-14 13:10:52 +0900

    bind: bpf_probe_read

--- furui-ebpf/src/bind.rs
+++ furui-ebpf/src/bind.rs
@@ -1,21 +1,28 @@
 #![no_std]
 #![no_main]
 
+use core::mem;
+use core::mem::MaybeUninit;
+
 use aya_bpf::{
-    cty::c_char,
-    helpers::{bpf_get_current_task, bpf_probe_read},
+    cty::{c_char, c_void},
+    helpers::{bpf_get_current_task, bpf_probe_read, gen},
     macros::{kprobe, map},
-    maps::PerfEventArray,
+    maps::{HashMap, PerfEventArray},
     programs::ProbeContext,
     BpfContext,
 };
+use aya_log_ebpf::info;
 
-use furui_common::BindEvent;
+use furui_common::{BindEvent, PortKey, PortVal};
 
 use crate::vmlinux::{socket, task_struct};
 
 #[allow(warnings)]
 mod vmlinux;
+
+#[map]
+static mut HASH: HashMap<PortKey, PortVal> = HashMap::with_max_entries(1024, 0);
 
 #[map]
 static mut BIND_EVENTS: PerfEventArray<BindEvent> =
@@ -30,40 +37,31 @@ pub fn bind_v4(ctx: ProbeContext) -> u32 {
 }
 
 unsafe fn try_bind_v4(ctx: ProbeContext) -> Result<u32, u32> {
-    let _ = (ctx.arg::<*const socket>(0).unwrap()).as_ref().unwrap();
+    let mut event = MaybeUninit::<BindEvent>::uninit();
 
-    let pid = ctx.pid();
-    let comm = ctx.command().unwrap();
+    (*event.as_mut_ptr()).pid = ctx.pid();
+    (*event.as_mut_ptr()).comm = ctx.command().unwrap();
 
-    let task = (bpf_get_current_task() as *const task_struct)
-        .as_ref()
-        .unwrap();
+    let task = &*(bpf_get_current_task() as *const task_struct);
 
-    let nsproxy = bpf_probe_read(&task.nsproxy).unwrap().as_ref().unwrap();
-    let pidns = bpf_probe_read(&nsproxy.pid_ns_for_children)
-        .unwrap()
-        .as_ref()
-        .unwrap();
+    let nsproxy = &*bpf_probe_read(&task.nsproxy).unwrap();
+    let pidns = &*bpf_probe_read(&nsproxy.pid_ns_for_children).unwrap();
 
     if bpf_probe_read(&pidns.level).unwrap() == 0 {
         return Ok(0);
     }
 
-    let uts = bpf_probe_read(&nsproxy.uts_ns).unwrap().as_ref().unwrap();
+    let uts = &*bpf_probe_read(&nsproxy.uts_ns).unwrap();
 
-    let container_id: [c_char; 65] = bpf_probe_read(&uts.name.nodename).unwrap();
-    let container_id = (&container_id[0..12])
-        .as_ptr()
-        .cast::<[c_char; 12]>()
-        .read();
+    gen::bpf_probe_read(
+        (*event.as_mut_ptr()).container_id.as_mut_ptr() as *mut c_void,
+        mem::size_of::<[c_char; 16]>() as u32,
+        uts.name.nodename.as_ptr() as *const c_void,
+    );
 
-    let event = BindEvent {
-        container_id,
-        pid,
-        comm,
-    };
+    let _ = (ctx.arg::<*const socket>(0).unwrap()).as_ref().unwrap();
 
-    BIND_EVENTS.output(&ctx, &event, 0);
+    BIND_EVENTS.output(&ctx, event.assume_init_ref(), 0);
 
     Ok(0)
 }
