3b8315be1af3f018b1f4f644a79aeca99400d9c8
Kensuke Kubo <kensukekubo19@gmail.com>
2022-06-18 22:29:38 +0900

    feat: common lib

--- furui-common/src/event/connect.rs
+++ furui-common/src/event/connect.rs
@@ -9,31 +9,6 @@ use crate::CONTAINER_ID_LEN;
 
 #[derive(Copy, Clone)]
 #[repr(C)]
-pub struct BindEvent {
-    // Only 12 characters are in the nodename, so it's 12+termination characters,
-    // but I've set it to 16 for memory alignment reasons.
-    pub container_id: [c_char; CONTAINER_ID_LEN],
-    pub pid: u32,
-    pub comm: [c_char; TASK_COMM_LEN],
-    pub family: c_ushort,
-    pub lport: c_ushort,
-    // // Defined at the very end for memory alignment.
-    pub protocol: u8,
-}
-
-#[cfg(feature = "user")]
-impl BindEvent {
-    pub fn family(&self) -> &'static str {
-        family(self.family)
-    }
-
-    pub fn protocol(&self) -> &'static str {
-        protocol(self.protocol)
-    }
-}
-
-#[derive(Copy, Clone)]
-#[repr(C)]
 pub struct ConnectEvent {
     // Only 12 characters are in the nodename, so it's 12+termination characters,
     // but I've set it to 16 for memory alignment reasons.
@@ -106,7 +81,6 @@ impl Connect6Event {
 mod user {
     use super::*;
 
-    unsafe impl aya::Pod for BindEvent {}
     unsafe impl aya::Pod for ConnectEvent {}
     unsafe impl aya::Pod for Connect6Event {}
 }
