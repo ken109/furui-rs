0769ea30becddcad9c55ac2b5584a5d224ae0f20
Kensuke Kubo <kensukekubo19@gmail.com>
2022-06-27 18:46:48 +0900

    fix: dont dereference

--- furui-ebpf/src/helpers/mod.rs
+++ furui-ebpf/src/helpers/mod.rs
@@ -12,22 +12,22 @@ mod tc;
 
 #[inline]
 pub(crate) unsafe fn is_container_process() -> Result<bool, c_long> {
-    let task = &*(bpf_get_current_task() as *const task_struct);
+    let task = bpf_get_current_task() as *const task_struct;
 
-    let nsproxy = &*bpf_probe_read_kernel(&task.nsproxy)?;
-    let pidns = &*bpf_probe_read_kernel(&nsproxy.pid_ns_for_children)?;
+    let nsproxy = bpf_probe_read_kernel(&(*task).nsproxy)?;
+    let pidns = bpf_probe_read_kernel(&(*nsproxy).pid_ns_for_children)?;
 
-    return Ok(bpf_probe_read_kernel(&pidns.level)? > 0);
+    return Ok(bpf_probe_read_kernel(&(*pidns).level)? > 0);
 }
 
 #[inline]
 pub(crate) unsafe fn get_container_id() -> Result<[c_char; CONTAINER_ID_LEN], c_long> {
-    let task = &*(bpf_get_current_task() as *const task_struct);
+    let task = bpf_get_current_task() as *const task_struct;
 
-    let nsproxy = &*bpf_probe_read_kernel(&task.nsproxy)?;
-    let uts = &*bpf_probe_read_kernel(&nsproxy.uts_ns)?;
+    let nsproxy = bpf_probe_read_kernel(&(*task).nsproxy)?;
+    let uts = bpf_probe_read_kernel(&(*nsproxy).uts_ns)?;
 
-    return Ok(*bpf_probe_read_kernel(&uts.name.nodename)?
+    return Ok(*bpf_probe_read_kernel(&(*uts).name.nodename)?
         .as_ptr()
         .cast::<[c_char; CONTAINER_ID_LEN]>());
 }
--- furui-ebpf/src/helpers/tc.rs
+++ furui-ebpf/src/helpers/tc.rs
@@ -7,12 +7,14 @@ use furui_common::{EthProtocol, IpProtocol};
 use crate::helpers::{ntohs, ETH_HDR_LEN};
 use crate::vmlinux::{ethhdr, iphdr, ipv6hdr};
 
+#[inline]
 pub(crate) fn eth_protocol(ctx: &SkBuffContext) -> Result<EthProtocol, c_long> {
     let eth = ctx.load::<ethhdr>(0)?;
 
     Ok(EthProtocol::from_eth(ntohs(eth.h_proto)))
 }
 
+#[inline]
 pub(crate) fn ip_protocol(ctx: &SkBuffContext) -> Result<IpProtocol, c_long> {
     match eth_protocol(ctx)? {
         EthProtocol::IP => {
