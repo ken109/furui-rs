df4ecc9170806e4e9f776c514dc4488486d8844b
Kensuke Kubo <kensukekubo19@gmail.com>
2022-06-27 18:46:48 +0900

    fix: dont dereference

--- furui-ebpf/src/helpers/net.rs
+++ furui-ebpf/src/helpers/net.rs
@@ -3,10 +3,12 @@ use crate::vmlinux::{ethhdr, iphdr};
 pub(crate) const ETH_HDR_LEN: usize = core::mem::size_of::<ethhdr>();
 pub(crate) const IP_HDR_LEN: usize = core::mem::size_of::<iphdr>();
 
+#[inline]
 pub(crate) fn ntohs(value: u16) -> u16 {
     u16::from_be(value)
 }
 
+#[inline]
 pub(crate) fn ntohl(value: u32) -> u32 {
     u32::from_be(value)
 }
