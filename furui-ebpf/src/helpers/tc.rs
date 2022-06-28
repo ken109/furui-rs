d2d7d847aaf00be157554e01ed027201efd04ecb
Kensuke Kubo <kensukekubo19@gmail.com>
2022-06-28 17:22:29 +0900

    refactor: ingress.rs

--- furui-ebpf/src/helpers/tc.rs
+++ furui-ebpf/src/helpers/tc.rs
@@ -4,8 +4,8 @@ use aya_bpf::programs::SkBuffContext;
 
 use furui_common::{EthProtocol, IpProtocol};
 
-use crate::helpers::{ntohs, ETH_HDR_LEN};
-use crate::vmlinux::{ethhdr, iphdr, ipv6hdr};
+use crate::helpers::{ntohs, ETH_HDR_LEN, IPV6_HDR_LEN, IP_HDR_LEN};
+use crate::vmlinux::{ethhdr, iphdr, ipv6hdr, tcphdr, udphdr};
 
 #[inline]
 pub(crate) fn eth_protocol(ctx: &SkBuffContext) -> Result<EthProtocol, c_long> {
@@ -29,4 +29,24 @@ pub(crate) fn ip_protocol(ctx: &SkBuffContext) -> Result<IpProtocol, c_long> {
         }
         EthProtocol::Other => Err(TC_ACT_OK as c_long),
     }
+}
+
+pub(crate) unsafe fn get_port(ctx: &SkBuffContext) -> Result<(u16, u16), c_long> {
+    let ip_hdr_len = match eth_protocol(ctx)? {
+        EthProtocol::IP => IP_HDR_LEN,
+        EthProtocol::IPv6 => IPV6_HDR_LEN,
+        EthProtocol::Other => return Err(TC_ACT_OK as c_long),
+    };
+
+    return match ip_protocol(ctx)? {
+        IpProtocol::TCP => {
+            let tcph = ctx.load::<tcphdr>(ETH_HDR_LEN + ip_hdr_len)?;
+            Ok((ntohs(tcph.source), ntohs(tcph.dest)))
+        }
+        IpProtocol::UDP => {
+            let udph = ctx.load::<udphdr>(ETH_HDR_LEN + ip_hdr_len)?;
+            Ok((ntohs(udph.source), ntohs(udph.dest)))
+        }
+        _ => Err(TC_ACT_OK as c_long),
+    };
 }
---
+++ furui-ebpf/src/ingress/ipv6_icmp.rs
@@ -0,0 +1,7 @@
+use aya_bpf::bindings::TC_ACT_OK;
+use aya_bpf::cty::c_long;
+use aya_bpf::programs::SkBuffContext;
+
+pub(crate) unsafe fn ipv6_icmp(_ctx: &SkBuffContext) -> Result<i32, c_long> {
+    Ok(TC_ACT_OK)
+}
